d29c712a3acb81639e5aba3708fbde7c9aadfe11
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-01-18 00:19:26 +0100

    test: add test cases for account entity add transaction method

--- tests/domain/test_account.py
+++ tests/domain/test_account.py
@@ -1,5 +1,21 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+from app.domain import Currency, Category, Account
 
 
 class TestAccount(TestCase):
-    pass
+    
+    def test_add_transaction_must_append_to_account(self):
+        my_account = Account("uuid", Currency.EUR, [])
+        t1 = patch("app.domain.Transaction")
+        t1.currency = Currency.EUR
+        my_account.add_transaction(t1)
+        self.assertEqual(my_account.transactions, [t1])
+
+    def test_add_transaction_must_raise_exception(self):
+        my_account = Account("uuid", Currency.USD, [])
+        t1 = patch("app.domain.Transaction")
+        t1.currency = Currency.EUR
+        with self.assertRaises(ValueError):
+            my_account.add_transaction(t1)
