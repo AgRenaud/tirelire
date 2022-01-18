76caad4569af1b7c848900b82effc063aad8ebc1
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-01-18 14:34:05 +0100

    Feat/add domain entities (#3)
    * feat: add Transaction, Category, Currency and Account entities to app.domain
    
    * feat: add balance methods
    
    * feat: add coverage scripts for unittest
    
    * test: test cases for Value app.domain entity
    
    * test: add test cases for account entity add transaction method
    
    * fix: test cases for transaction
    
    * feat: add test account tests cases
    
    * fix: typo
    
    * feat: add date to Transaction
    
    * test: add test case for balance computation

---
+++ app/domain/account.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import List
+
+from app.domain.transaction import Transaction, Value
+from app.domain.currency import Currency
+from app.domain.category import Category
+
+
+@dataclass
+class Account:
+    id: str
+    currency: Currency
+    transactions: List[Transaction]
+
+    def add_transaction(self, new_transaction: Transaction) -> None:
+        if not new_transaction.currency == self.currency:
+            raise ValueError(
+                f"Wrong currency ! Need {self.currency} get {new_transaction.currency}")
+        self.transactions.append(new_transaction)
+
+    def compute_balance(self) -> Value:
+        balance = Value(0.0, self.currency)
+        for transaction in self.transactions:
+            balance += transaction.value
+        return balance
+
+    def compute_category_balance(self, category: Category) -> Value:
+        balance = Value(0.0, self.currency)
+        category_transactions = [
+            transaction for transaction in self.transactions 
+            if transaction.category == category
+        ] 
+        for transaction in category_transactions:
+            balance += transaction.value
+        return balance
