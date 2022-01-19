abd0458df963270e6e489fddb76ed94b42e595d7
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-01-19 23:04:09 +0100

    test: tests cases for exception for account and transaction entities

--- app/domain/account/__init__.py
+++ app/domain/account/__init__.py
@@ -1,6 +1,6 @@
 from app.domain.account.account import Account
 from app.domain.account.account_exceptions import (
     AccountNotFoundError,
-    AccountNotFoundError,
+    AccountsNotFoundError,
 )
 from app.domain.account.account_repository import AccountRepository
