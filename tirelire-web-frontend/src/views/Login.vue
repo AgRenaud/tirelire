fe8977f1e6daf7c2578cd4be657dacbb81df25fa
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-03-12 23:18:17 +0100

    feat: add form components for sign up and sign in

--- tirelire-web-frontend/src/views/Login.vue
+++ tirelire-web-frontend/src/views/Login.vue
@@ -1,39 +1,46 @@
 <template>
-  <div class="login-container">
+  <div class="view-container">
     <div class="carousel-container">
-      Hello world
+      <img src="@/assets/img-main-features-presentation.svg">
+      <h1>Suivez vos comptes avec Tirelire ! 🐽</h1>
     </div>
     <div class="form-container">
-      <FormAuth />
+      <FormSignIn />
     </div>
   </div>
 </template>
 
 <script setup>
-  import FormAuth from '@/components/forms/FormAuth'
+  import FormSignIn from '@/components/forms/FormSignIn'
 </script>
 
 <style scoped>
-  .login-container {
+  .view-container {
     display: flex;
-    height: 100vh;
-    text-align: center;
+    flex-direction: row;
+    height: 100%;
+  }
+  .carousel-container, .form-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: auto;
   }
   .carousel-container {
-    height: inherit;
-    width: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    flex-direction: column;
+    color: white;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.4));
     background-color: #42b983;
     border: 1px solid rgb(0, 134, 78);
-    box-shadow: 6px 0 5px -2px #888;
+    box-shadow: 6px 0 5px -2px rgb(136, 136, 136, .5);
   }
+  /*
   .form-container {
     height: inherit;
     width: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
-  }
+  } */
 </style>
