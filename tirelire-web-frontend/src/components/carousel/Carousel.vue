96ec50daf0df3fe939b9ddd84663db365b8e7cd4
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-03-13 14:43:07 +0100

    feat: add carousel on sign in/up page

--- tirelire-web-frontend/src/components/carousel/Carousel.vue
+++ tirelire-web-frontend/src/components/carousel/Carousel.vue
@@ -1,57 +1,113 @@
 <template>
   <div class="carousel">
-    <slot />
-    <button
-      class="carousel__nav carousel__next" 
-      @click.prevent="next"
-    >
-      Next
-    </button>
-    <button
-      class="carousel__nav carousel__prev" 
-      @click.prevent="prev"
-    >
-      Previous
-    </button>
+    <slot :currentSlide="currentSlide" />
+
+    <!-- Pagination -->
+    <div v-if="pagintationEnabled" class="pagination">
+      <span
+        @click="goToSlide(index)"
+        v-for="(slide, index) in getSlideCount"
+        :key="index"
+        :class="{ active: index + 1 === currentSlide }"
+      >
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-
+import { ref, onMounted } from "vue";
 export default {
-    data () {
-        return {
-            index: 0,
-            slides: []
-        }
-    },
-    computed: {
-      slidesCount () { return this.slides.length}
-    },
-    mounted () {
-        this.slides = this.$children
-        this.slides.forEach((slide, i) => {
-            slide.index = i
-        })
-    },
-    methods: {
-      next () {
-        this.index++
-        if (this.index >= this.slidesCount) {
-          this.index = this.slidesCount - 1 
-        }
-      },
-      prev () {
-        this.index-- 
-        if (this.index < this.slidesCount) {
-          this.index = 0
-        }
+  props: ["startAutoPlay", "timeout", "navigation", "pagination"],
+  setup(props) {
+    const currentSlide = ref(1);
+    const getSlideCount = ref(null);
+    const autoPlayEnabled = ref(
+      props.startAutoPlay === undefined ? true : props.startAutoPlay
+    );
+    const timeoutDuration = ref(props.timeout === undefined ? 5000 : props.timeout);
+    const pagintationEnabled = ref(
+      props.pagination === undefined ? true : props.pagination
+    );
+    const navEnabled = ref(props.navigation === undefined ? true : props.navigation);
+    // next slide
+    const nextSlide = () => {
+      if (currentSlide.value === getSlideCount.value) {
+        currentSlide.value = 1;
+        return;
       }
+      currentSlide.value += 1;
+    };
+    // prev slide
+    const prevSlide = () => {
+      if (currentSlide.value === 1) {
+        currentSlide.value = 1;
+        return;
+      }
+      currentSlide.value -= 1;
+    };
+    const goToSlide = (index) => {
+      currentSlide.value = index + 1;
+    };
+    // autoplay
+    const autoPlay = () => {
+      setInterval(() => {
+        nextSlide();
+      }, timeoutDuration.value);
+    };
+    if (autoPlayEnabled.value) {
+      autoPlay();
     }
-}
-
+    onMounted(() => {
+      getSlideCount.value = document.querySelectorAll(".slide").length;
+    });
+    return {
+      currentSlide,
+      nextSlide,
+      prevSlide,
+      getSlideCount,
+      goToSlide,
+      pagintationEnabled,
+      navEnabled,
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.toggle-page {
+    display: flex;
+    flex: 1;
+  }
+i {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    background-color: #6347c7;
+    color: #fff;
+}
+.pagination {
+  position: relative;
+  bottom: 0px;
+  width: 100%;
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+}
+span {
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+}
+.active {
+    background-color: #035d14;
+}
 </style>
