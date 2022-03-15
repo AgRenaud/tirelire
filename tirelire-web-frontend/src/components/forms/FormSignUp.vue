36c953beb22b9b65f58dafd8051ecc616b058fbb
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-03-15 08:46:45 +0100

    feat: bind form component to api call

--- tirelire-web-frontend/src/components/forms/FormSignUp.vue
+++ tirelire-web-frontend/src/components/forms/FormSignUp.vue
@@ -15,42 +15,41 @@
   })
 
   function signUp () {
-      console.log(formInput)
+      const backend_url = import.meta.env.VITE_BACKEND_URL
       return fetch(
-          'http://jsonplaceholder.typicode.com/posts', {
+          backend_url + '/api/v1/register', {
           method: 'post',
           headers: {
               'content-type': 'application/json'
           }
-        })
-        .then(res => {
-        // a non-200 response code
-        if (!res.ok) {
-            // create error instance with HTTP status text
-            const error = new Error(res.statusText);
-            error.json = res.json();
-            throw error;
-        }
-
-        return res.json();
-        })
-        .then(json => {
-        // set the response data
-        data.value = json.data;
-        })
-        .catch(err => {
-        error.value = err;
-        // In case a custom JSON error response was provided
-        if (err.json) {
-            return err.json.then(json => {
-            // set the JSON response message
-            error.value.message = json.message;
-            });
-        }
-        })
-        .then(() => {
-        loading.value = false;
         });
+      //  .then(res => {
+      //  // a non-200 response code
+      //  if (!res.ok) {
+      //      // create error instance with HTTP status text
+      //      const error = new Error(res.statusText);
+      //      error.json = res.json();
+      //      throw error;
+      //  }
+      //  return res.json();
+      //  })
+      //  .then(json => {
+      //  // set the response data
+      //  data.value = json.data;
+      //  })
+      //  .catch(err => {
+      //  error.value = err;
+      //  // In case a custom JSON error response was provided
+      //  if (err.json) {
+      //      return err.json.then(json => {
+      //      // set the JSON response message
+      //      error.value.message = json.message;
+      //      });
+      //  }
+      //  })
+      //  .then(() => {
+      //  loading.value = false;
+      //  });
     }
 </script>
 
--- tirelire-web-frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-let config;
-
-config = {
-    $backend_url: process.env.BACKEND_URL,
-  };
