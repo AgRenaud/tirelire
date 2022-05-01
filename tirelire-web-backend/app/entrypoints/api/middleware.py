d99a6e5474c107eec87079926c8244c7444e6c04
Renaud Aguirre <renaud.aguirrechambilla@gmail.com>
2022-05-01 11:05:38 +0200

    feat: handle not connected behaviour

--- tirelire-web-backend/app/entrypoints/api/middleware.py
+++ tirelire-web-backend/app/entrypoints/api/middleware.py
@@ -1,6 +1,9 @@
 from fastapi import Request
+from fastapi.responses import JSONResponse
 
 
 async def auth_middleware(request: Request, call_next):
     response = await call_next(request)
+    if response.status_code == 401:
+        return JSONResponse(status_code=401, payload={"message": "Not connected."})
     return response
