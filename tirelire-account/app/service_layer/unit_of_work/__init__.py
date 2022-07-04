1554cc204c38babfd5fe91f6e98953895b48040d
Renaud Aguirre <renaud.aguirrechambilla@gmail.com>
2022-07-04 07:38:20 +0200

    fix: model

---
+++ tirelire-account/app/service_layer/unit_of_work/__init__.py
@@ -0,0 +1,2 @@
+from app.service_layer.unit_of_work.unit_of_work import AbstractUnitOfWork
+from app.service_layer.unit_of_work.sqlalchemy_uow import SQLAlchemyUnitOfWork
---
+++ tirelire-account/app/service_layer/unit_of_work/sqlalchemy_uow.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+
+from app.service_layer.unit_of_work.unit_of_work import AbstractUnitOfWork
+from app.service_layer.session_factory import DEFAULT_SESSION_FACTORY
+from app.adapters.repository.holder_repository import HolderRepositoryImplem
+
+
+class SQLAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session: Session = self.session_factory()
+        self.holders = HolderRepositoryImplem(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def _commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
