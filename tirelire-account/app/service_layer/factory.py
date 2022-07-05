84ca3aad66ad0cc0b737d65a6066a4c8024f486b
Renaud Aguirre <renaud.aguirrechambilla@gmail.com>
2022-07-05 17:21:28 +0200

    fix: make everything work again

--- tirelire-account/app/service_layer/factory.py
+++ tirelire-account/app/service_layer/factory.py
@@ -9,3 +9,5 @@ from app.service_layer.session_factory import DEFAULT_SESSION_FACTORY
 SQL_ALCHEMY_UOW_FACTORY = partial(SQLAlchemyUnitOfWork, DEFAULT_SESSION_FACTORY)
 
 REDIS_FACTORY = partial(Redis, decode_responses=True, **config.get_redis_config())
+
+REDIS_LISTENER_FACTORY = partial(Redis, decode_responses=True, **config.get_redis_config())
